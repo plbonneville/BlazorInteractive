8188df8441137d90a4292d2f51760ed03f00a5b5
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2021-06-05 17:13:21 -0400

    Moved the unit tests in the same project

--- tests/BlazorInteractive.Tests/BlazorInteractiveTests.cs
+++ tests/BlazorInteractive.Tests/BlazorInteractiveTests.cs
@@ -180,5 +180,103 @@ namespace BlazorInteractive.Tests
                 .Should()
                 .Contain("<h1>hello world</h1>");
         }
+
+        [Fact]
+        public async Task It_can_reference_a_component_defined_in_previous_compilation()
+        {
+            // Arrange
+            await _kernel.SubmitCodeAsync(@"#!blazor
+                         <h1>hello world</h1>");
+
+            using var events = _kernel.KernelEvents.ToSubscribedList();
+
+            // Act
+            await _kernel.SubmitCodeAsync(@"typeof(__Main).Name");
+
+            // Assert
+            events
+                .Should()
+                .ContainSingle<DisplayEvent>()
+                .Which
+                .FormattedValues
+                .Should()
+                .ContainSingle(v => v.MimeType == "text/plain")
+                .Which
+                .Value
+                .Should()
+                .Be("__Main");
+        }
+
+        [Fact]
+        public async Task It_can_instantiate_a_component_defined_in_previous_compilation()
+        {
+            // Arrange
+            await _kernel.SubmitCodeAsync(@"#!blazor
+                         <h1>hello world</h1>");
+
+            using var events = _kernel.KernelEvents.ToSubscribedList();
+
+            // Act
+            await _kernel.SubmitCodeAsync(@"var component = new __Main();");
+
+            // Assert
+            events
+                .Should()
+                .ContainSingle<CommandSucceeded>()
+                .Which
+                .Command.As<SubmitCode>()
+                .Code
+                .Should()
+                .Be("var component = new __Main();");
+        }
+
+        [Fact]
+        public async Task It_can_reference_a_named_component_defined_in_previous_compilation()
+        {
+            // Arrange
+            await _kernel.SubmitCodeAsync(@"#!blazor --name HelloWorld
+                         <h1>hello world</h1>");
+
+            using var events = _kernel.KernelEvents.ToSubscribedList();
+
+            // Act
+            await _kernel.SubmitCodeAsync(@"typeof(HelloWorld).Name");
+
+            // Assert
+            events
+                .Should()
+                .ContainSingle<DisplayEvent>()
+                .Which
+                .FormattedValues
+                .Should()
+                .ContainSingle(v => v.MimeType == "text/plain")
+                .Which
+                .Value
+                .Should()
+                .Be("HelloWorld");
+        }
+
+        [Fact]
+        public async Task It_can_instantiate_a_named_component_defined_in_previous_compilation()
+        {
+            // Arrange
+            await _kernel.SubmitCodeAsync(@"#!blazor -n HelloWorld
+                         <h1>hello world</h1>");
+
+            using var events = _kernel.KernelEvents.ToSubscribedList();
+
+            // Act
+            await _kernel.SubmitCodeAsync(@"var component = new HelloWorld();");
+
+            // Assert
+            events
+                .Should()
+                .ContainSingle<CommandSucceeded>()
+                .Which
+                .Command.As<SubmitCode>()
+                .Code
+                .Should()
+                .Be("var component = new HelloWorld();");
+        }
     }
 }
