0031d5aa78a24a5eaba2083b8fe6b430409c143e
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-12-26 19:58:18 -0500

    Converted to a record type

--- src/BlazorInteractive/BlazorMarkdown.cs
+++ src/BlazorInteractive/BlazorMarkdown.cs
@@ -1,15 +1,6 @@
 ﻿namespace BlazorInteractive;
 
-public class BlazorMarkdown
-{
-    public BlazorMarkdown(string value, string componentName)
-    {
-        Value = value;
-        ComponentName = componentName;
-    }
-
-    public string Value { get; }
-    public string ComponentName { get; }
-
+internal sealed record BlazorMarkdown(string Value, string ComponentName)
+{ 
     public override string ToString() => Value;
 }
