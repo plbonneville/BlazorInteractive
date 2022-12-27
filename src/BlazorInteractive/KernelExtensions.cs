6377252a000e1d90ad1b82b4dde4b7df44066a38
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-12-27 11:30:44 -0500

    Replace Kernel by CompositeKernel

--- src/BlazorInteractive/KernelExtensions.cs
+++ src/BlazorInteractive/KernelExtensions.cs
@@ -15,12 +15,12 @@ internal static class KernelExtensions
     private static readonly Assembly[] _references =
     {
         typeof(ComponentBase).Assembly,   // Microsoft.AspNetCore.Components
-        typeof(WebAssemblyHost).Assembly, // Microsoft.AspNetCore.Components.WebAssembly                    
+        typeof(WebAssemblyHost).Assembly, // Microsoft.AspNetCore.Components.WebAssembly
         typeof(DataType).Assembly,        // System.ComponentModel.DataAnnotations
         typeof(JsonContent).Assembly      // System.Net.Http.Json
     };
 
-    internal static Task LoadRequiredAssemblies(this Kernel kernel)
+    internal static Task LoadRequiredAssemblies(this CompositeKernel kernel)
         => LoadRequiredAssemblies(kernel.FindKernelByName("csharp") as CSharpKernel);
 
     private static async Task LoadRequiredAssemblies(this CSharpKernel csharpKernel)
@@ -28,6 +28,6 @@ internal static class KernelExtensions
         var rDirectives = string.Join(Environment.NewLine, _references.Select(a => $"#r \"{a.Location}\""));
 
         await csharpKernel.SendAsync(new SubmitCode($"{rDirectives}"), CancellationToken.None).ConfigureAwait(false);
-        //await csharpKernel.SendAsync(new SubmitCode($"{rDirectives}{Environment.NewLine}{usings}"), CancellationToken.None).ConfigureAwait(false);           
+        //await csharpKernel.SendAsync(new SubmitCode($"{rDirectives}{Environment.NewLine}{usings}"), CancellationToken.None).ConfigureAwait(false);
     }
 }
